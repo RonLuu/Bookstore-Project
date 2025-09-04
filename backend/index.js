f29527abb320dd223c6d25882c542f9944f7386f
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-05 07:42:37 +1000

    Save a book with Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,38 @@
 import express, { request } from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
-
+import {Book} from "./models/bookModel.js"
 const app = express();
+
+app.use(express.json())
 
 app.get("/", (req, res) =>{
     console.log(req)
     return res.status(234).send("Wecome to MERN Stack Tutorial")
 })
 
+app.post("/books", async (req, res) => {
+    try{
+        if (!req.body.title || !req.body.author || !req.body.publishYear) 
+        {
+            return res.status(400).send({
+                message: "Send all required fields: title, author, publishYear",
+            })
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        }
+        const book = await Book.create(newBook)
+        return res.status(201).send(book)
+    }
+    catch (error) {
+        console.log(error.message)
+        console.log(req.body)
+        res.status(500).send({message: error.message})
+    }
+})
 
 mongoose.connect(mongoDBURL).then(() => {
     console.log("App connected to database")
