b99267662f557a05a3c6c7f0decf3a6b17ecff75
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-08 23:49:40 +1000

    Fix the URL database

--- Backend/config.js
+++ Backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5000
-export const MONGO_DB_URL = "mongodb + srv://ronluu:MonGoDB%233110@bookstore-mern.rgudwv1.mongodb.net/"
+export const MONGO_DB_URL = "mongodb+srv://ronluu:MonGoDB%233110@bookstore-mern.0jtkdyu.mongodb.net/books_collection?retryWrites=true&w=majority&appName=Bookstore-MERN"
