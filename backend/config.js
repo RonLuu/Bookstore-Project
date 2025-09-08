786c3086ece0ab595f232198ad7c66be6220e456
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-08 21:51:51 +1000

    Reset Project

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-export const mongoDBURL =
-'mongodb+srv://root:BookStoreMERN%233110@bookstore-mern.c31tqvr.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN'
