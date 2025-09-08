553129ab74bd800dd59220286e59b70717e1448a
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-09 08:17:10 +1000

    Add a way to post a book

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,40 @@
 import express from "express"
 import { MONGO_DB_URL, PORT } from "./config.js"
 import mongoose from "mongoose"
+import { Book } from "./models/bookModel.js"
 
 const app = express()
+app.use(express.json()) // Middleware to parse request body
 
 app.get("/", (req,res) =>{
     console.log(req)
     return res.status(200).send("Welcome to MERN stack")
 })
 
+app.post("/books", async (req, res)=>{
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear)
+        {
+            return res.status(400).send({
+                message: "Send all the required fields: title, author, publishYear"
+            })
+        }
+        // Book object in js
+        const js_book = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        }
+
+        const mongo_book = await Book.create(js_book)
+        console.log(mongo_book)
+        return res.status(200).send(mongo_book)
+    }
+    catch (error){
+        console.log(error.message)
+        res.status(500).send({message: error.message})
+    }
+})
 
 mongoose
     .connect(MONGO_DB_URL)
