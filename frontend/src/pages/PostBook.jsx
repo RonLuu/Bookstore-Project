a0175d40bf25a0659c912d82e44c6bb622dc30fe
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-08 21:52:09 +1000

    Reset Project

--- frontend/src/pages/PostBook.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PostBook = () => {
-  return (
-    <div>PostBook</div>
-  )
-}
-
-export default PostBook
--- frontend/src/pages/PutBook.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PutBook = () => {
-  return (
-    <div>UpdateBook</div>
-  )
-}
-
-export default UpdateBook
